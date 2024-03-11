494c24ccbdfcf0627411578cf784f6f82195aced
NemethRajmondAdam <152845824+NemethRajmondAdam@users.noreply.github.com>
2024-03-11 12:43:32 +0100

    valmi

--- index.php
+++ index.php
@@ -4,26 +4,81 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>Raktár Rendszer</title>
 </head>
-<body>
-    <h1>Üdvözlöm!</h1>
-
+<body class="hatter">
+    <header>
+        <div class="cim"><h1>Üdvözlöm!</h1></div>
+    </header>
     <?php
         require_once('osztaly.php');
 
         $tabla = new Osztaly();
 
-        $epuletek = $tabla->getLocal();
-
-        foreach ($epuletek as $epulet) {
-            echo '<form method="POST" action="">'
-            .'<tr>'
-                .'<td> <button  class="" value="'.$epulet['rakatr_id'].'" name="">'.$epulet['epulet'].'</button> </td>'
-                . '<td><div style="display: flex">'
-                . '</div></td>'
-            . '</tr></form>';
-        }
     ?>
+    <div class="container">
+        <div class="half">
+            <?php
+                $epuletek = $tabla->getStores();
+
+                foreach ($epuletek as $epulet) {
+                    echo '<form method="POST" action="">'
+                    .'<tr>'
+                        .'<td> <button  class="button" value="'.$epulet['id'].'" name="store_buttons">'.$epulet['name'].'</button> </td>'
+                        . '<td><div style="display: flex">'
+                        . '</div></td>'
+                    . '</tr></form>';
+                }
+            ?>
+        </div>
+        <div class="half">
+            <?php
+            if (isset($_POST['store_buttons'])) {
+                $id = $_POST['store_buttons'];
+                $termekek = $tabla->getProductsById($id);
+
+                if (!empty($termekek)) {
+                    $sv = 0;
+                    foreach ($termekek as $termek)
+                    {
+                        if ($sv<1) {
+                            $store = $tabla->getStoreById($termek['id_store']);
+                            echo '<td class="alcim">'.$store.' </td><br>';
+
+                        } 
+                            echo '<form method="POST" action="">'
+                            .'<tr>'
+                                //.'<td class="cim">'.$store.' </td>'
+                                .'<td>'.$termek['name'].' </td>'
+                                .'<td>'.$termek['price'].'FT </td>'
+                                .'<td>'.$termek['quantity'].'db </td>'
+                                . '<td><div style="display: flex">'
+                                . '</div></td>'
+                            . '</tr></form>';
+
+                        $sv+=1;
+                            
+                    }
+                }
+            }
+            ?>
+        </div>
+    </div>
+    <div class ="container">
+        <div class="half">
+            <h2 class="alcim">Leltár készítő</h2>
+            <?php
+                $raktarok = $tabla->getStores();
+
+                echo '<div class="custom-select"><select>';
+                foreach ($raktarok as $raktar) {
+                    echo '<option value="'.$raktar['id'].'">'.$raktar['name'].'</option>';
+                }
+                echo '</select><br>';
+                echo '<button  class="button" value="" name="leltar">Leltár készítése</button></div>';
+            ?>
+        </div>
+    </div>
 </body>
 </html>
--- osztaly.php
+++ osztaly.php
@@ -16,11 +16,29 @@ class Osztaly{
             $this->mysqli->close();
         }
 
-        public function getLocal(): array
+        public function getStores(): array
         {
-            $query="SELECT * FROM raktarok";
+            $query="SELECT * FROM stores";
 
             return $this->mysqli->query($query)->fetch_all(MYSQLI_ASSOC);
+        }
+
+        public function getProductsById($id): array
+        {
+            $query="SELECT * FROM products WHERE id_store = $id";
+
+            return $this->mysqli->query($query)->fetch_all(MYSQLI_ASSOC);
+        }
+
+        public function getStoreById($id)
+        {
+            $result = $this->mysqli->query("SELECT name FROM stores WHERE id = $id");
+            $store = $result->fetch_assoc();
+            if (isset($store['name'])) {
+                return $store['name'];    
+            }
+            return "";
+ 
         }
 }
 ?>
