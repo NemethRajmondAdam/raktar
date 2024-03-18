a23521165ccd86a2337f0b472c9865313c4fdcbd
NemethRajmondAdam <152845824+NemethRajmondAdam@users.noreply.github.com>
2024-03-18 14:39:45 +0100

    asd

--- index.php
+++ index.php
@@ -49,7 +49,6 @@
                         } 
                             echo '<form method="POST" action="">'
                             .'<tr>'
-                                //.'<td class="cim">'.$store.' </td>'
                                 .'<td>'.$termek['name'].' </td>'
                                 .'<td>'.$termek['price'].'FT </td>'
                                 .'<td>'.$termek['quantity'].'db </td>'
@@ -71,13 +70,91 @@
             <?php
                 $raktarok = $tabla->getStores();
 
-                echo '<div class="custom-select"><select>';
+
+                echo '<form method="POST" action=""><select class="custom-select" name="raktarokOpciok">';
                 foreach ($raktarok as $raktar) {
                     echo '<option value="'.$raktar['id'].'">'.$raktar['name'].'</option>';
                 }
                 echo '</select><br>';
-                echo '<button  class="button" value="" name="leltar">Leltár készítése</button></div>';
+                //echo '<input class="button" type="submit" name="leltar" value="Leltár Készítés">';
+                echo '<button class="button" type="submit" name="leltar">Leltár Készítése</button></form>';
+               
             ?>
+        </div>
+        <div class="half">
+            <?php
+                if (isset($_POST['leltar'])) {
+                    $storeId = $_POST['raktarokOpciok'];
+
+                    $eredmenyek = $tabla->getAllFromProducts($storeId);
+                    foreach ($eredmenyek as $eredmeny) {
+                        $sorS = $tabla->getRows($eredmeny['id_row']);
+                        $oszlopS = $tabla->getColls($eredmeny['id_column']);
+                        $polcS = $tabla->getShelves($eredmeny['id_shelf']);
+
+                        echo 'A '.$eredmeny['name'].'-ből található '.$eredmeny['quantity'].' db a(z) '.$sorS.' sor '.$oszlopS.' oszlopjának '.$polcS.' polcán<br>';
+                    }
+                }
+            ?>
+        </div>
+    </div>
+    <div class="container">
+        <div class="half">
+                <h2 class="alcim">Új adat</h2>
+                <?php
+                $raktarok = $tabla->getStores();
+                $sorok = $tabla->getAllRows();
+                $oszlopok = $tabla->getAllCols();
+                $polcok = $tabla->getAllShelves();
+
+
+                echo '<form method="POST" action=""><select class="custom-select" name="raktarokOpciokUj">';
+                foreach ($raktarok as $raktar) {
+                    echo '<option value="'.$raktar['id'].'">'.$raktar['name'].'</option>';
+                }
+                echo '</select><br>';
+                echo '<select class="custom-select" name="sorok">';
+                foreach ($sorok as $sor) {
+                    echo '<option value="'.$sor['id'].'">'.$sor['name'].'</option>';
+                }
+                echo '</select><br>';
+                echo '<select class="custom-select" name="oszlopok">';
+                foreach ($oszlopok as $oszlop) {
+                    echo '<option value="'.$oszlop['id'].'">'.$oszlop['name'].'</option>';
+                }
+                echo '</select><br>';
+                echo '<select class="custom-select" name="polcok">';
+                foreach ($polcok as $polc) {
+                    echo '<option value="'.$polc['id'].'">'.$polc['name'].'</option>';
+                }
+                echo '</select><br>';
+                echo'<input type="text" id="nev" name="nev" placeholder="Termék neve"><br>
+                    <input type="number" id="ar" name="ar" placeholder="Termék ára"><br>
+                    <input type="number" id="db" name="db" placeholder="Darabszáma"><br>
+                    <input type="number" id="min_db" name="min_db" placeholder="Minimum mennyiség"><br>';
+                    
+                echo '<button class="button" type="submit" name="newData">Adatok mentése</button></form>';
+
+                if (isset($_POST['newData'])) {
+                    $raktar = $_POST['raktarokOpciokUj'];
+                    $sor = $_POST['sorok'];
+                    $oszlop = $_POST['oszlopok'];
+                    $polc = $_POST['polcok'];
+                    $nev = $_POST['nev'];
+                    $ar = $_POST['ar'];
+                    $db = $_POST['db'];
+                    $min = $_POST['min_db'];
+
+                    $tabla->new($raktar,$sor,$oszlop,$polc,$nev,$ar,$db,$min);
+                }
+               
+            ?>
+        </div>
+        <div class="half">
+            <h2 class="alcim">Módosítás</h2>
+        </div>
+        <div class="half">
+            <h2 class="alcim">Törlés</h2>
         </div>
     </div>
 </body>
--- osztaly.php
+++ osztaly.php
@@ -38,7 +38,89 @@ class Osztaly{
                 return $store['name'];    
             }
             return "";
- 
         }
+        
+        public function getAllFromProducts($id)
+        {
+            $query="SELECT * FROM products WHERE id_store = $id";
+
+            return $this->mysqli->query($query)->fetch_all(MYSQLI_ASSOC);
+        }
+
+        public function getRows($id)
+        {            
+            $result = $this->mysqli->query("SELECT name FROM row WHERE id = $id");
+            $rows = $result->fetch_assoc();
+            if (isset($rows['name'])) {
+                return $rows['name'];    
+            }
+            return "";
+        }
+
+        public function getColls($id)
+        {            
+            $result = $this->mysqli->query("SELECT name FROM columns WHERE id = $id");
+            $cols = $result->fetch_assoc();
+            if (isset($cols['name'])) {
+                return $cols['name'];    
+            }
+            return "";
+        }
+        public function getShelves($id)
+        {            
+            $result = $this->mysqli->query("SELECT name FROM shelves WHERE id = $id");
+            $shelves = $result->fetch_assoc();
+            if (isset($shelves['name'])) {
+                return $shelves['name'];    
+            }
+            return "";
+        }
+
+        public function getAllRows():array
+        {
+            $query="SELECT * FROM row";
+
+            return $this->mysqli->query($query)->fetch_all(MYSQLI_ASSOC);
+        }
+
+        public function getAllCols():array
+        {
+            $query="SELECT * FROM columns";
+
+            return $this->mysqli->query($query)->fetch_all(MYSQLI_ASSOC);
+        }
+
+        public function getAllShelves():array
+        {
+            $query="SELECT * FROM shelves";
+
+            return $this->mysqli->query($query)->fetch_all(MYSQLI_ASSOC);
+        }
+
+        public function Keres($string):array
+        {
+            $query = "SELECT * FROM products WHERE name = '$string'";
+
+            return $this->mysqli->query($query)->fetch_all(MYSQLI_ASSOC);
+        }
+
+        public function new($raktar,$sor,$oszlop,$polc,$nev,$ar,$db,$minDb){
+            if (empty($this->Keres($nev))) {
+                $query="INSERT INTO products (id_store,id_row,id_column,id_shelf,name,price,quantity,min_quantity ) VALUES ($raktar, $sor, $oszlop, $polc, '$nev', $ar, $db, $minDb)";
+
+                if ($this->mysqli->query($query) == TRUE) {
+                    echo "Az adatok sikeresen feltöltve.";
+                }
+                else {
+                    echo "Hiba az adatok feltöltése közben.". $this->mysqli->error;
+                }
+            }
+            else{
+                echo"";
+            }
+
+        }
+
 }
+
 ?>
